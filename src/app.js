1bb61b2ee53a74374d7175f9a4987c989fe20716
InHouse <studioinhouse.gt@gmail.com>
2021-06-06 12:51:40 -0600

    Creación de la estructura de carptetas para proyecto GS Trophy y carga de header con menu toggle desplegable para mobile.

---
+++ src/app.js
@@ -0,0 +1,6 @@
+function cambiarClase(){
+    let siteNav = document.getElementById('site-nav');
+        siteNav.classList.toggle('site-nav-open');
+    let menuClose = document.getElementById('menu-toggle-close');
+        menuClose.classList.toggle('menu-toggle--open');
+}
