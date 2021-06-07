b37a614c381f8a6c00ee29d7be4eabc36ce3461e
InHouse <studioinhouse.gt@gmail.com>
2021-06-06 21:50:48 -0600

    Desarrollo de caja flotante informativa de las reglas de participación para el GS Trophy Qualifier GT

--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
-function cambiarClase(){
+let closePopup;
+let cambiarClase;
+let dialogPopupOpen;
+
+cambiarClase=()=>{
     let siteNav = document.getElementById('site-nav');
         siteNav.classList.toggle('site-nav-open');
     let menuClose = document.getElementById('menu-toggle-close');
         menuClose.classList.toggle('menu-toggle--open');
 }
+
+dialogPopupOpen=()=>{
+    let sitePopupOpen = document.getElementById('open');
+         sitePopupOpen.style.display="block"
+    let sitePopupDialog = document.getElementById('dialog-pop-up');
+        sitePopupDialog.style.display="block";     
+}
+
+closePopup=()=>{
+    let sitePopup = document.getElementById('dialog-pop-up');
+        sitePopup.style.display="none";
+}
+
