0ebe6010c1adff8757a02b00ed9ca157b81f69a5
InHouse <studioinhouse.gt@gmail.com>
2021-06-07 20:48:33 -0600

    actualización de ficheros

--- src/app.js
+++ src/app.js
@@ -1,22 +1,23 @@
-let closePopup;
-let cambiarClase;
-let dialogPopupOpen;
+const slider = document.querySelectorAll(".container-rules");
 
-cambiarClase=()=>{
+console.log(slider);
+
+
+let cambiarClase=()=>{
     let siteNav = document.getElementById('site-nav');
         siteNav.classList.toggle('site-nav-open');
     let menuClose = document.getElementById('menu-toggle-close');
         menuClose.classList.toggle('menu-toggle--open');
 }
 
-dialogPopupOpen=()=>{
+let dialogPopupOpen=()=>{
     let sitePopupOpen = document.getElementById('open');
          sitePopupOpen.style.display="block"
     let sitePopupDialog = document.getElementById('dialog-pop-up');
         sitePopupDialog.style.display="block";     
 }
 
-closePopup=()=>{
+let = closePopup=()=>{
     let sitePopup = document.getElementById('dialog-pop-up');
         sitePopup.style.display="none";
 }
