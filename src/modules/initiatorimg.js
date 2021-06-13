475d35e4b501599d5c218120192c9574f977d5af
InHouse <studioinhouse.gt@gmail.com>
2021-06-13 12:04:37 -0600

    Carga de actualización de imágenes del segundo día

--- src/modules/initiatorimg.js
+++ src/modules/initiatorimg.js
@@ -1,8 +1,10 @@
-import { bikerImageDay } from './objectModel.js';
+import { bikerImageDay, bikerImageDay2 } from './objectModel.js';
 
 let constructorDisplayImage= ()=>{
     const constructorSectionImg = document.getElementById('constructor-show-img');
+    const constructorSectionImg2 = document.getElementById('constructor-show-img2');
     const fragmentImg = document.createDocumentFragment();
+    const fragmentImg2 = document.createDocumentFragment();
 
     bikerImageDay.forEach(positionimg =>{
         const imageShow = document.createElement('img');
@@ -16,6 +18,19 @@ let constructorDisplayImage= ()=>{
     });
 
     constructorSectionImg.appendChild(fragmentImg);
+
+    bikerImageDay2.forEach(positionimg2 =>{
+        const imageShow2 = document.createElement('img');
+
+        imageShow2.classList = `container-scroll-img__section`;
+        imageShow2.src=`${positionimg2.img}`;
+        imageShow2.alt =`Imagen de cobertura del día 1 del evento GS Trophy Qualifier GT 2021`;
+        
+        fragmentImg2.appendChild(imageShow2);       
+
+    });
+
+    constructorSectionImg2.appendChild(fragmentImg2);
 }
 
 constructorDisplayImage();
